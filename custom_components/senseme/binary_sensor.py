38fbf9a569c8d5d35dd53a502b82ead7e29ddd46
J. Nick Koston <nick@koston.org>
2021-02-22 07:15:42 -1000

    Cleanups for senseme devices

--- custom_components/senseme/binary_sensor.py
+++ custom_components/senseme/binary_sensor.py
@@ -6,6 +6,7 @@ from homeassistant.components.binary_sensor import (
     BinarySensorEntity,
 )
 
+from . import SensemeEntity
 from .const import DOMAIN, UPDATE_RATE
 
 _LOGGER = logging.getLogger(__name__)
@@ -13,8 +14,6 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, entry, async_add_entities):
     """Set up SenseME occupancy sensors."""
-    if hass.data.get(DOMAIN) is None:
-        hass.data[DOMAIN] = {}
     if hass.data[DOMAIN].get("sensor_devices") is None:
         hass.data[DOMAIN]["sensor_devices"] = []
 
@@ -35,60 +34,21 @@ async def async_setup_entry(hass, entry, async_add_entities):
     hass.data[DOMAIN]["discovery"].add_callback(async_discovered_devices)
 
 
-class HASensemeOccupancySensor(BinarySensorEntity):
+class HASensemeOccupancySensor(SensemeEntity, BinarySensorEntity):
     """Representation of a Big Ass Fans SenseME occupancy sensor."""
 
     def __init__(self, device):
         """Initialize the entity."""
-        self.device = device
-        self._name = device.name + " Occupancy"
+        super().__init__(device, f"{device.name} Occupancy")
 
     async def async_added_to_hass(self):
         """Add data updated listener after this object has been initialized."""
         self.device.add_callback(self.async_write_ha_state)
 
     @property
-    def name(self):
-        """Get sensor name."""
-        return self._name
-
-    @property
-    def device_info(self):
-        """Get device info for Home Assistant."""
-        info = {
-            "connections": {("mac", self.device.id)},
-            "identifiers": {("token", self.device.network_token)},
-            "name": self.device.name,
-            "manufacturer": "Big Ass Fans",
-            "model": self.device.model,
-        }
-        if self.device.fw_version:
-            info["sw_version"] = self.device.fw_version
-        return info
-
-    @property
     def unique_id(self):
         """Return a unique identifier for this sensor."""
-        uid = f"{self.device.id}-SENSOR"
-        return uid
-
-    @property
-    def should_poll(self) -> bool:
-        """Sensor state is pushed."""
-        return False
-
-    @property
-    def device_state_attributes(self) -> dict:
-        """Get the current state attributes."""
-        attributes = {}
-        if self.device.room_status:
-            attributes["room"] = self.device.room_name
-        return attributes
-
-    @property
-    def available(self) -> bool:
-        """Return True if available/operational."""
-        return self.device.connected
+        return f"{self.device.id}-SENSOR"
 
     @property
     def is_on(self) -> bool:
